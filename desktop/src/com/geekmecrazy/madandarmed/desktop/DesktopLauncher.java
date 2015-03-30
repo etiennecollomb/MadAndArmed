105e42aa6c4e5d68ae704764f5f9597abea3ea44
eco <etienne.collomb@gmail.com>
2015-03-30 16:51:56 +0200

    First commit

---
+++ desktop/src/com/geekmecrazy/madandarmed/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.geekmecrazy.madandarmed.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.geekmecrazy.madandarmed.MadAndArmed;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MadAndArmed(), config);
+	}
+}
