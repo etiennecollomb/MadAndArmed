1e2806e7c811a859dfb9b9115d43286b01f5b5f4
Etienne Collomb <eco@geekmecrazy.com>
2015-05-11 20:27:52 +0200

    1

--- core/src/com/geekmecrazy/madandarmed/IA/IsoMapState.java
+++ core/src/com/geekmecrazy/madandarmed/IA/IsoMapState.java
@@ -89,7 +89,7 @@ public class IsoMapState {
 	
 	public boolean isUpOccupied(final int pX, final int pY){
 		if(pY/2*2==pY) return isOccupied(pX+1, pY+1); //Pair
-		return isOccupied(pX, pY+1); //Impair
+		return isOccupied(pX, pY+1); //Impair 
 	}
 	public boolean isDownOccupied(final int pX, final int pY){
 		if(pY/2*2==pY) return isOccupied(pX, pY-1); //Pair
