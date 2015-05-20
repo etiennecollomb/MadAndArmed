7ce89ecbb2cb7f1087c0b055d7201c8d0f3dbaa3
Etienne Collomb <eco@geekmecrazy.com>
2015-05-20 11:57:05 +0200

    1

--- core/src/com/geekmecrazy/madandarmed/Json/DataLoader.java
+++ core/src/com/geekmecrazy/madandarmed/Json/DataLoader.java
@@ -5,20 +5,20 @@ import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.ObjectMap;
 import com.geekmecrazy.madandarmed.Core.GlobalManager;
-import com.geekmecrazy.madandarmed.Pattern.BuildingPattern;
-import com.geekmecrazy.madandarmed.Pattern.BuildingPattern.BuildingType;
+import com.geekmecrazy.madandarmed.Pattern.BuildingPattern2;
+import com.geekmecrazy.madandarmed.Pattern.BuildingPattern2.BuildingType;
 import com.geekmecrazy.madandarmed.Pattern.WeaponPattern;
 import com.geekmecrazy.madandarmed.Pattern.WeaponPattern.WeaponType;
 
 public class DataLoader {
 
 	/**TEST*/
-	static ObjectMap<String, PhoneNumber> buildingPatternMap = new ObjectMap<String, PhoneNumber>();
+	static ObjectMap<String, BuildingPattern2> buildingPatternMap = new ObjectMap<String, BuildingPattern2>();
 	/**FIN TEST*/
 
 
 
-	private static ObjectMap<BuildingType, BuildingPattern> buildingsPattern;
+	private static ObjectMap<BuildingType, BuildingPattern2> buildingsPattern;
 	private static ObjectMap<WeaponType, WeaponPattern> weaponsPattern;
 
 
@@ -32,8 +32,7 @@ public class DataLoader {
 
 	@SuppressWarnings("unchecked")
 	public static void loadBuildingsPattern() { 
-		try {
-			
+
 			/** TEST */
 //			Json json = new Json();
 //			FileHandle text = Gdx.files.internal(GlobalManager.JSON_BUILDINGSPATTERN);
@@ -41,15 +40,21 @@ public class DataLoader {
 //			System.out.println(buildingPatternMap.get("CASTEL"));
 			
 			Json json = new Json();
+			
+			/*
+			BuildingPattern2 bp2 = new BuildingPattern2();
+			bp2.name = "t1";
+			bp2.setBuildingType(BuildingType.CASTLE);
+			System.out.println(json.prettyPrint(bp2));
+			String text = json.toJson(bp2, Object.class);
+			BuildingPattern2 bp3 = json.fromJson(BuildingPattern2.class, text);
+			*/
+			
 			FileHandle text = Gdx.files.internal(GlobalManager.JSON_BUILDINGSPATTERN);
 			buildingPatternMap = json.fromJson(ObjectMap.class, text);
 			System.out.println(buildingPatternMap.get("first"));
 			/**FIN TEST*/
 
-		} catch(Exception e) { 
-			System.out.println("### Error DataLoader.java - loadBuildingsPattern()");
-			System.out.println(e.getMessage());
-		}
 	}
 
 	// ===========================================================
